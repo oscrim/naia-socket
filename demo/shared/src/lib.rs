20f1adbc52ea2dece078a9b231b9ed3b4c3f6da6
Connor Carpenter <naia.rust.crate@gmail.com>
2021-09-13 18:08:50 -0600

    cleanup

--- demo/shared/src/lib.rs
+++ demo/shared/src/lib.rs
@@ -1,13 +1,2 @@
-use std::net::SocketAddr;
-
-mod shared_config;
-pub use shared_config::get_shared_config;
-
-pub fn get_server_address() -> SocketAddr {
-    return "127.0.0.1:14191"
-        .parse()
-        .expect("could not parse socket address from string");
-}
-
-pub const PING_MSG: &str = "ping";
-pub const PONG_MSG: &str = "pong";
+mod shared;
+pub use shared::{get_server_address, get_shared_config, PING_MSG, PONG_MSG};
--- demo/shared/src/shared.rs
+++ demo/shared/src/shared.rs
@@ -1,4 +1,15 @@
+use std::net::SocketAddr;
+
 use naia_socket_shared::{LinkConditionerConfig, SocketConfig};
+
+pub const PING_MSG: &str = "ping";
+pub const PONG_MSG: &str = "pong";
+
+pub fn get_server_address() -> SocketAddr {
+    return "127.0.0.1:14191"
+        .parse()
+        .expect("could not parse socket address from string");
+}
 
 pub fn get_shared_config() -> SocketConfig {
     //let link_condition = None;
